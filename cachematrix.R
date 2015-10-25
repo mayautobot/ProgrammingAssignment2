67f66ee5002ecc7266dbdc507b02d9f6f51a0484
Mayank Gupta <mayautobot@localhost.localdomain>
2015-10-26 01:16:58 +0530

    Added the definitions for makeCacheMatrix and cacheSolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## this function returns the list of getter and setter function for invertible matrix and inverse of that invertible matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	##variable holding inverse of the matrix: x
+  invM <- NULL
+  
+  ##setting the invertible matrix x
+  setMatrix <- function(y){
+    x <<- y
+    invM <<- NULL
+  }
+  
+  ##returning the invertible matrix x
+  getMatrix <- function(){
+    x
+  }
+  
+  ##setting the inverse of the invertible matrix x
+  setInverse <- function(inverseM){
+    invM <<- inverseM
 }
 
 
-## Write a short comment describing this function
+##function for caching inverse of the invertible matrix using solve function if the inverse
+##of the matrix is not already set, i.e., inverse of the matrix is null
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Returns a matrix that is the inverse of 'x'
+  
+	##getting inverse of the invertible matrix
+  invM <- x$getInverse()
+  
+  ##checking if the got inverse is null or not, if not returns it
+  if(!is.null(invM)){
+    message("getting cached inverse of the matrix")
+    return(invM)
+  }
+  
+  
+  sourceMatrix <- x$getMatrix()
+  
+  ##calculates inverse of the invertible matrix using solve()
+  invM <- solve(sourceMatrix, ...)
+  
+  x$setInverse(invM)
+  
+  invM
 }
